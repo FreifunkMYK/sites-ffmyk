72d42650f4b6338a8bc9598f8bd8765477751b22
Niklas Yann Wettengel <niyawe@niyawe.de>
2017-07-15 16:17:03 +0200

    fixed map, removed broken qrcode

--- site.mk
+++ site.mk
@@ -10,7 +10,6 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
 	gluon-config-mode-geo-location-map \
-	gluon-config-mode-qrcode \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
 	gluon-ebtables-filter-multicast \
