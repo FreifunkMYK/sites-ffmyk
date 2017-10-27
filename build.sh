30332010226ac5e95b4882b28c53a460db244460
Niklas Yann Wettengel <niyawe@niyawe.de>
2017-10-27 18:45:39 +0200

    added build script

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SCRIPTPATH="$(dirname "$(readlink -e "$0")" )"
+
+SITES="ffaw ffcoc ffems ffko ffmy ffsim"
+TARGETS="ar71xx-generic ar71xx-tiny ar71xx-nand brcm2708-bcm2708 brcm2708-bcm2709 mpc85xx-generic x86-generic x86-geode x86-64"
+
+CORES=$(nproc)
+MAKEOPTS="-j$((CORES+1))"
+
+AUTOUPDATER_PRIORITY=10
+
+RELEASE=$1
+
+rm -rf $SCRIPTPATH/output/$RELEASE
+mkdir -p $SCRIPTPATH/output/$RELEASE/packages
+
+for SITE in $SITES; do
+	ln -s ${SCRIPTPATH}/sites/${SITE} ${SCRIPTPATH}/gluon/site
+	cd gluon
+	make update
+	for TARGET in ${TARGETS}; do
+		echo ${SITE} ${TARGET}
+		make ${MAKEOPTS} GLUON_TARGET=${TARGET} GLUON_RELEASE=${RELEASE} GLUON_BRANCH=stable
+	done
+	make GLUON_BRANCH=autoupdater GLUON_PRIORITY=${AUTOUPDATER_PRIORITY} GLUON_RELEASE=${RELEASE} manifest
+	make GLUON_BRANCH=stable GLUON_RELEASE=${RELEASE} manifest
+	mv output/images/sysupgrade/autoupdater.manifest ${SCRIPTPATH}/output/$RELEASE/${SITE}.autoupdater.manifest
+	mv output/images/sysupgrade/stable.manifest ${SCRIPTPATH}/output/$RELEASE/${SITE}.stable.manifest
+	mv output/packages/* ${SCRIPTPATH}/output/$RELEASE/packages/
+	mv output/images ${SCRIPTPATH}/output/$RELEASE/${SITE}
+	rm -f site
+	cd ..
+done
