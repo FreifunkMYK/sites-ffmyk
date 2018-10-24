ae5a029c3f18eac1b01f5a7be663c92a9d933876
Niklas Yann Wettengel <niyawe@niyawe.de>
2018-10-24 20:09:04 +0200

    added config-mode-domain-select

--- site.mk
+++ site.mk
@@ -8,6 +8,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-contact-info \
 	gluon-config-mode-core \
+	gluon-config-mode-domain-select \
 	gluon-config-mode-geo-location-map \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
