390306baef3eed8decc6f53af6da2243b5a0fb20
Niklas Yann Wettengel <niyawe@niyawe.de>
2021-04-18 15:07:49 +0200

    prepare: download for all targets

--- prepare.sh
+++ prepare.sh
@@ -3,10 +3,15 @@ set -x
 
 SCRIPTPATH="$(dirname "$(readlink -e "$0")" )"
 
+CORES=$(nproc)
+
 ln -s ${SCRIPTPATH} ${SCRIPTPATH}/gluon/site
 cd gluon
 for patch in ${SCRIPTPATH}/patches/*.patch; do
     git apply $patch
 done
+TARGETS="$(make list-targets)"
 make update
-make download
+for TARGET in ${TARGETS}; do
+    make -j$((CORES+1)) GLUON_TARGET=${TARGET} download
+done
