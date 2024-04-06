6ba419084dc638270b7a4effbfec5b3c5d251e07
Niklas Yann Wettengel <niyawe@niyawe.de>
2024-04-06 02:41:14 +0200

    disable tls for more devices

--- image-customization.lua
+++ image-customization.lua
@@ -171,6 +171,7 @@ local exclude_tls = {
     'tp-link-tl-wdr3600-v1',
     'tp-link-tl-wdr4300-v1',
     'tp-link-tl-wr1043nd-v2',
+    'tp-link-tl-wr1043nd-v3',
     'tp-link-tl-wr2543n-nd',
     'tp-link-tl-wr810n-v1',
     'tp-link-tl-wr841n-v13',
