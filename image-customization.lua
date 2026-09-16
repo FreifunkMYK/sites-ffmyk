0460bac4b702488add51feefcb803f91ba04c9bf
Florian Knodt <git@adlerweb.info>
2026-09-16 21:11:46 +0200

    kmod-fs-ntfs → kmod-fs-ntfs3
    Old version was removed upstream: https://github.com/openwrt/openwrt/commit/43c48d09ecaa1c0950906d60a4968060f57594c9

--- image-customization.lua
+++ image-customization.lua
@@ -51,7 +51,7 @@ local pkgs_usb_storage = {
     'block-mount',
     'blkid',
     'kmod-fs-ext4',
-    'kmod-fs-ntfs',
+    'kmod-fs-ntfs3',
     'kmod-fs-vfat',
     'kmod-usb-storage',
     'kmod-usb-storage-extras',  -- Card Readers
